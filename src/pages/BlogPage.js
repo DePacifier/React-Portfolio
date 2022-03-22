41a0673eb484fab208f1f390362517846ad25b96
DePacifier <milkybekele@gmail.com>
2022-03-22 09:48:10 +0300

    finished working on the blog page

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+// Component Imports
+import LogoComponent from "../components/LogoComponent";
+import PowerButton from "../components/PowerButton";
+import SocialIcons from "../components/SocialIcons";
+import BlogComponent from "../components/BlogComponent";
+
+// Image Imports
+import backgroundImg from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg";
+
+// Blog Data Import
+import Blogs from "../data/BlogData";
+import AnchorComponent from "../components/AnchorComponent";
+
+const MainContainer = styled.div`
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+  width: 100%;
+  height: auto;
+  position: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
 
 const BlogPage = () => {
-  return <div>BlogPage Component page</div>;
+  const [numbers, setNumbers] = useState(0);
+
+  useEffect(() => {
+    let num = (window.innerHeight - 70) / 30;
+    setNumbers(parseInt(num));
+  }, []);
+
+  return (
+    <MainContainer>
+      <Container>
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
+        <AnchorComponent numbers={numbers} />
+
+        <Center>
+          <Grid>
+            {Blogs.map((blog) => (
+              <BlogComponent key={blog.id} blog={blog} />
+            ))}
+          </Grid>
+        </Center>
+      </Container>
+    </MainContainer>
+  );
 };
 
 export default BlogPage;
