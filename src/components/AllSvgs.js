b1c0d15ab048241710002b6ebcda19edd20426a0
DePacifier <milkybekele@gmail.com>
2022-03-22 10:37:56 +0300

    finished working on skills page

--- src/components/AllSvgs.js
+++ src/components/AllSvgs.js
@@ -200,3 +200,20 @@ export const Develope = (props) => {
     </svg>
   );
 };
+
+export const Robot = (props) => (
+  <svg
+    aria-hidden="true"
+    data-prefix="fas"
+    data-icon="robot"
+    className="prefix__svg-inline--fa prefix__fa-robot prefix__fa-w-20"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 640 512"
+    {...props}
+  >
+    <path
+      fill={props.fill}
+      d="M32 224h32v192H32a31.962 31.962 0 0 1-32-32V256a31.962 31.962 0 0 1 32-32Zm512-48v272a64.063 64.063 0 0 1-64 64H160a64.063 64.063 0 0 1-64-64V176a79.974 79.974 0 0 1 80-80h112V32a32 32 0 0 1 64 0v64h112a79.974 79.974 0 0 1 80 80Zm-280 80a40 40 0 1 0-40 40 39.997 39.997 0 0 0 40-40Zm-8 128h-64v32h64Zm96 0h-64v32h64Zm104-128a40 40 0 1 0-40 40 39.997 39.997 0 0 0 40-40Zm-8 128h-64v32h64Zm192-128v128a31.962 31.962 0 0 1-32 32h-32V224h32a31.962 31.962 0 0 1 32 32Z"
+    />
+  </svg>
+);
--- src/pages/MySkillsPage.js
+++ src/pages/MySkillsPage.js
@@ -1,7 +1,141 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+
+// Component Imports
+import { Robot, Develope } from "../components/AllSvgs";
+import LogoComponent from "../components/LogoComponent";
+import PowerButton from "../components/PowerButton";
+import SocialIcons from "../components/SocialIcons";
+import ParticleComponent from "../components/ParticleComponent";
+
+// Styling Imports
+import { lightTheme } from "./Themes";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  cursor: pointer;
+
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1rem + 1vw);
+
+  ${Main}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
+    }
+  }
+
+  & > *:first-child {
+    margin-right: 1rem;
+  }
+`;
+
+const Description = styled.div`
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.6em + 1vw);
+  padding: 0.5rem 0;
+
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+  }
+
+  strong {
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+  }
+
+  ul,
+  p {
+    margin-left: 2rem;
+  }
+`;
 
 const MySkillsPage = () => {
-  return <div>MySkillsPage Component page</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Box>
+        <LogoComponent theme="light" />
+        <SocialIcons theme="light" />
+        <PowerButton />
+        <ParticleComponent theme="light" />
+        <Main>
+          <Title>
+            <Develope width={40} height={40} /> Front-End Developer
+          </Title>
+          <Description>
+            I love to create design which speaks, Keep it clean, minimal and
+            simple.
+          </Description>
+          <Description>
+            <strong>I like to Design</strong>
+            <ul>
+              <li>Web Design</li>
+              <li>Mobile Apps</li>
+            </ul>
+          </Description>
+          <Description>
+            <strong>Tools</strong>
+            <ul>
+              <li>Figma</li>
+            </ul>
+          </Description>
+        </Main>
+        <Main>
+          <Title>
+            <Robot width={40} height={40} /> Data Analyst and ML Engineer
+          </Title>
+          <Description>
+            I value business or brand for which i'm creating, thus i enjoy
+            bringing new ideas to life.
+          </Description>
+          <Description>
+            <strong>Skills</strong>
+            <p>
+              Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase
+              etc.
+            </p>
+          </Description>
+          <Description>
+            <strong>Tools</strong>
+            <ul>
+              <p>VScode, Github, Codepen etc.</p>
+            </ul>
+          </Description>
+        </Main>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default MySkillsPage;
